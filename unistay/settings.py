f8d684ed6c8718a311aea8a285e474002ea4c933
pappi02 <pappichulo2002.com>
2024-12-05 08:16:19 +0300

    static good

--- unistay/settings.py
+++ unistay/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY', default='your_default_secret_key')
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 # Allowed Hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['localhost', '127.0.0.1', '0.0.0.0'])
 
 # Application definition
 INSTALLED_APPS = [
